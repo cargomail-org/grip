82e38abff4b636972c3c48c83007bd621d6a74e4
1token <zboran@federizer.com>
2023-03-11 19:37:03 +0100

    rfc4954/ jwt

--- poc/smtpd/test/send-mtls.sh
+++ poc/smtpd/test/send-mtls.sh
@@ -3,5 +3,6 @@ curl \
 --cert ../../cert/client.crt \
 --url "smtp://bar.127.0.0.2.nip.io:2525" --ssl -k \
 --mail-from myself@example.com \
+--mail-auth eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c \
 --mail-rcpt receiver@example.com \
 --upload-file email.txt
