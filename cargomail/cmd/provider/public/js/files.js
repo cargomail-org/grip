116f33cb810f09856650cd83ac4d7260dbf0e635
1token <zboran@federizer.com>
2023-07-14 16:28:01 +0200

    files

--- cargomail/cmd/provider/public/js/files.js
+++ cargomail/cmd/provider/public/js/files.js
@@ -1,10 +1,20 @@
 import DataTable from "datatables.net";
 
+// import $ from 'jquery';
+
 import "datatables.net-bs5";
 import "datatables.net-select";
 import "datatables.net-select-bs5";
+import "datatables.net-buttons";
+import "datatables.net-buttons-bs5";
 import "datatables.net-responsive";
 import "datatables.net-responsive-bs5";
+
+let selectedIds = [];
+
+const confirmDialog = new bootstrap.Modal(
+  document.querySelector("#confirmDialog")
+);
 
 const table = new DataTable("#files-table", {
   paging: true,
@@ -18,7 +28,13 @@ const table = new DataTable("#files-table", {
   columns: [
     { data: "id", visible: false, searchable: false },
     { data: null, visible: true, orderable: false, width: "15px" },
-    { data: "name" },
+    {
+      data: "name",
+      render: (data, type, full, meta) => {
+        const link = "/api/v1/resource/";
+        return `<a href="${link}${full.uuid}">${data}</a>`;
+      }
+    },
     { data: "size", searchable: false },
   ],
   columnDefs: [
@@ -36,4 +52,63 @@ const table = new DataTable("#files-table", {
     info: true,
   },
   order: [[0, "desc"]],
+  dom: "Bfrtip",
+  buttons: [
+    {
+      text: "Reload",
+      action: function () {
+        table.ajax.reload();
+      },
+    },
+    {
+      text: "Delete selected",
+      action: function () {
+        selectedIds = [];
+
+        const selectedData = table
+          .rows(".selected")
+          .data()
+          .map((obj) => obj.id);
+        if (selectedData.length > 0) {
+          confirmDialog.show();
+          for (let i = 0; i < selectedData.length; i++) {
+            selectedIds.push(parseInt(selectedData[i], 10));
+          }
+        }
+      },
+    },
+  ],
 });
+
+export const deleteItems = (e) => {
+  e.preventDefault();
+
+  confirmDialog.hide();
+
+  console.log(selectedIds);
+
+  fetch("api/v1/resources/delete", {
+    method: "post",
+    body: JSON.stringify(selectedIds),
+  })
+    .then((response) => {
+      if (response.ok) {
+        table.rows(".selected").remove().draw();
+        console.log("Successfully deleted file(s)");
+        if (!Object.keys(response).length) {
+          console.log("no return data found");
+          return;
+        }
+        return response && response.json();
+      }
+      throw new Error("something went wrong");
+    })
+    .then((data) => {
+      if (data) {
+        console.log("File(s) deleted:", data);
+      }
+    })
+    .catch((error) => {
+      console.log("Error while deleting file(s):", error);
+    });
+};
--- cargomail/cmd/provider/public/js/index.js
+++ cargomail/cmd/provider/public/js/index.js
@@ -1,36 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    filesContent(); // in prod should be cargoesContent
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  filesContent(); // in prod should be cargoesContent
+});
 
 function composeContent(e) {
-    document.getElementById("compose-container").hidden = false;
-    document.getElementById("compose-link").classList.add("active");
+  document.getElementById("compose-container").hidden = false;
+  document.getElementById("compose-link").classList.add("active");
 
-    document.getElementById("cargoes-container").hidden = true;
-    document.getElementById("cargoes-link").classList.remove("active");
+  document.getElementById("cargoes-container").hidden = true;
+  document.getElementById("cargoes-link").classList.remove("active");
 
-    document.getElementById("files-container").hidden = true;
-    document.getElementById("files-link").classList.remove("active");
+  document.getElementById("files-container").hidden = true;
+  document.getElementById("files-link").classList.remove("active");
 }
 
 function cargoesContent(e) {
-    document.getElementById("compose-container").hidden = true;
-    document.getElementById("compose-link").classList.remove("active");
+  document.getElementById("compose-container").hidden = true;
+  document.getElementById("compose-link").classList.remove("active");
 
-    document.getElementById("cargoes-container").hidden = false;
-    document.getElementById("cargoes-link").classList.add("active");
+  document.getElementById("cargoes-container").hidden = false;
+  document.getElementById("cargoes-link").classList.add("active");
 
-    document.getElementById("files-container").hidden = true;
-    document.getElementById("files-link").classList.remove("active");
+  document.getElementById("files-container").hidden = true;
+  document.getElementById("files-link").classList.remove("active");
 }
 
 function filesContent(e) {
-    document.getElementById("compose-container").hidden = true;
-    document.getElementById("compose-link").classList.remove("active");
-    
-    document.getElementById("cargoes-container").hidden = true;
-    document.getElementById("cargoes-link").classList.remove("active");
-    
-    document.getElementById("files-container").hidden = false;
-    document.getElementById("files-link").classList.add("active");
+  document.getElementById("compose-container").hidden = true;
+  document.getElementById("compose-link").classList.remove("active");
+
+  document.getElementById("cargoes-container").hidden = true;
+  document.getElementById("cargoes-link").classList.remove("active");
+
+  document.getElementById("files-container").hidden = false;
+  document.getElementById("files-link").classList.add("active");
 }
